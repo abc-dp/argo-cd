73317eb76283b3efa4a276a86bb427a9f0b4faf8
Yujun Zhang <yujunz@nvidia.com>
2021-05-31 15:10:37 +0800

    custom: install cue

--- hack/custom/install.sh
+++ hack/custom/install.sh
@@ -6,16 +6,21 @@ apt-get update && \
   apt-get clean && \
   rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-export CURL_ARGS="-sSL --fail"
-export BIN=/usr/local/bin
+BIN=/usr/local/bin
 
-export SOPS_VERSION=v3.6.1
+CURL_WITH_ARGS="curl -sSL --fail"
+TAR_WITH_ARGS="tar zxf - -C ${BIN}"
 
-curl ${CURL_ARGS} -o ${BIN}/sops https://github.com/mozilla/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION}.linux
+SOPS_VERSION=v3.6.1
+${CURL_WITH_ARGS} -o ${BIN}/sops https://github.com/mozilla/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION}.linux
 chmod +x ${BIN}/sops
 
-export HELMFILE_VERSION=0.138.4-3-g90de61a
-export HELMFILE_RELEASE=https://github.com/yujunz/helmfile/releases/download/v${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION}_Linux_x86_64.tar.gz
-curl ${CURL_ARGS} ${HELMFILE_RELEASE} | tar zxf - -C ${BIN} helmfile
+HELMFILE_VERSION=0.138.4-3-g90de61a
+HELMFILE_RELEASE=https://github.com/yujunz/helmfile/releases/download/v${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION}_Linux_x86_64.tar.gz
+${CURL_WITH_ARGS} ${HELMFILE_RELEASE} | ${TAR_WITH_ARGS} helmfile
 
-pip3 install yq==2.12.0
+CUE_VERSION=v0.4.0
+${CURL_WITH_ARGS} https://github.com/cuelang/cue/releases/download/${CUE_VERSION}/cue_${CUE_VERSION}_linux_amd64.tar.gz | ${TAR_WITH_ARGS} cue
+
+YQ_VERSION=2.12.0
+pip3 install yq==${YQ_VERSION}
