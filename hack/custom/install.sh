38fffeb29ef7289c4c7018fa153c51967ff3a109
Yujun Zhang <yujunz@nvidia.com>
2021-05-31 15:18:34 +0800

    custom: upgrade helmfile

--- hack/custom/install.sh
+++ hack/custom/install.sh
@@ -15,9 +15,9 @@ SOPS_VERSION=v3.6.1
 ${CURL_WITH_ARGS} -o ${BIN}/sops https://github.com/mozilla/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION}.linux
 chmod +x ${BIN}/sops
 
-HELMFILE_VERSION=0.138.4-3-g90de61a
-HELMFILE_RELEASE=https://github.com/yujunz/helmfile/releases/download/v${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION}_Linux_x86_64.tar.gz
-${CURL_WITH_ARGS} ${HELMFILE_RELEASE} | ${TAR_WITH_ARGS} helmfile
+HELMFILE_VERSION=v0.139.7
+${CURL_WITH_ARGS} -o ${BIN}/helmfile https://github.com/roboll/helmfile/releases/download/${HELMFILE_VERSION}/helmfile_linux_amd64
+chmod +x ${BIN}/helmfile
 
 CUE_VERSION=v0.4.0
 ${CURL_WITH_ARGS} https://github.com/cuelang/cue/releases/download/${CUE_VERSION}/cue_${CUE_VERSION}_linux_amd64.tar.gz | ${TAR_WITH_ARGS} cue
