ad6333455911930a7e636b0245275fb603e58bbf
Yujun Zhang <yujunz@nvidia.com>
2021-08-02 17:09:30 +0800

    custom: support age as encryption tool

--- hack/custom/install.sh
+++ hack/custom/install.sh
@@ -11,7 +11,7 @@ BIN=/usr/local/bin
 CURL_WITH_ARGS="curl -sSL --fail"
 TAR_WITH_ARGS="tar zxf - -C ${BIN}"
 
-SOPS_VERSION=v3.6.1
+SOPS_VERSION=v3.7.1
 ${CURL_WITH_ARGS} -o ${BIN}/sops https://github.com/mozilla/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION}.linux
 chmod +x ${BIN}/sops
 
@@ -24,3 +24,6 @@ ${CURL_WITH_ARGS} https://github.com/cuelang/cue/releases/download/${CUE_VERSION
 
 YQ_VERSION=2.12.0
 pip3 install yq==${YQ_VERSION}
+
+AGE_VERSION=v1.0.0-rc.3
+${CURL_WITH_ARGS} https://github.com/FiloSottile/age/releases/download/${AGE_VERSION}/age-${AGE_VERSION}-linux-amd64.tar.gz | ${TAR_WITH_ARGS} --strip=1 age/age
--- hack/custom/user-init.sh
+++ hack/custom/user-init.sh
@@ -12,7 +12,7 @@ helm2 init --client-only && \
 
 export KUSTOMIZE_PLUGIN_PATH=${XDG_CONFIG_HOME}/kustomize/plugin
 
-export KSOPS_VERSION=2.2.2
+export KSOPS_VERSION=2.5.7
 export KSOPS_PATH=${KUSTOMIZE_PLUGIN_PATH}/viaduct.ai/v1/ksops
 mkdir -p $KSOPS_PATH
 curl -sSL --fail https://github.com/viaduct-ai/kustomize-sops/releases/download/v${KSOPS_VERSION}/ksops_${KSOPS_VERSION}_Linux_x86_64.tar.gz \
